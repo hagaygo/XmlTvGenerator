1bbcbfce96473ffec43ea6354455e39984a78dd1
hagaygo <hagay.goshen@gmail.com>
2013-11-11 22:59:48 +0200

    core library

---
+++ XmlTvGenerator.Core/Show.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlTvGenerator.Core
+{
+    public class Show
+    {
+        public Show Clone()
+        {
+            return (Show)MemberwiseClone();
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Channel { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration
+        {
+            get { return EndTime - StartTime; }
+        }
+
+        public int? Series { get; set; }
+        public int? Episode { get; set; }
+    }
+}
