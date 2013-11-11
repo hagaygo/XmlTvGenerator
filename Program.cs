099d7dfe71f36bf5584b937d21e4404e2c125e62
Hagay Goshen <hagaygo@users.noreply.github.com>
2013-11-11 22:47:10 +0200

    Update Program.cs
    test

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace XmlTvGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("test"); // ?
         }
     }
 }
