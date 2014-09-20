3c928aa1c4c2dbadf438e4831638def37ee4aef6
hagaygo <hagay.goshen@gmail.com>
2014-09-20 21:08:53 +0300

    added cyfrowy.pl channels

--- Grabbers/CyfrowyPolsat.pl/GrabParameters.cs
+++ Grabbers/CyfrowyPolsat.pl/GrabParameters.cs
@@ -28,6 +28,13 @@ namespace CyfrowyPolsat.pl
         al_jazeera,
         polsat_sport_extra_hd,
         polsat_sport_hd,
+        tvp_sport,
+        jim_jam_polsat,
+        bbc_cbeebies,
+        nsport_hd,
+        canal_plus_family2_hd,
+        canal_plus_family_hd,
+        canal_plus_sport_hd
     }
 
     public class GrabParameters : GrabParametersBase
