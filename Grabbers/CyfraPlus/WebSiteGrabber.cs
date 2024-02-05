94a34378f6129591eaf80275d0cc2284d056e456
hagaygo <hagay@HAGAY-PC>
2024-02-05 19:29:04 +0200

    update cyfra grabber

--- Grabbers/CyfraPlus/WebSiteGrabber.cs
+++ Grabbers/CyfraPlus/WebSiteGrabber.cs
@@ -12,129 +12,34 @@ using XmlTvGenerator.Core;
 namespace CyfraPlus
 {
     public class WebSiteGrabber : GrabberBase
-    {        
-        const string urlFormat = "https://pl.canalplus.com/iapi/tv-schedule/Schedule";
-        const string DateFormat = "yyyy-MM-dd";
-
-        List<Show> Grab(GrabParametersBase p, ILogger logger)
-        {
-            var pp = (CyfraPlus.GrabParameters)p;
-            var shows = new List<Show>();
-            var wr = WebRequest.Create(urlFormat);
-            wr.Method = "POST";
-            wr.ContentType = "application/json";
-            wr.Headers.Add("Sec-Fetch-Mode", "cors");
-            using (var sw = new StreamWriter(wr.GetRequestStream()))
-            {
-                sw.Write("{\"start_date\":\"" + pp.Date.ToString(DateFormat) + "T00:00:00\",\"end_date\":\"" + pp.Date.AddDays(1).ToString(DateFormat) + "T00:00:00\"}");
-            }
-            logger.WriteEntry(string.Format("Grabbing Cyfra+ date {0} ...", pp.Date.ToString(DateFormat)), LogType.Info);
-            var res = (HttpWebResponse)wr.GetResponse();
-
-            using (var sr = new StreamReader(res.GetResponseStream()))
-            {
-                var startDownloadTime = DateTime.Now;
-                var data = new StringBuilder();
-                int blockSize = 16384;
-                while (!sr.EndOfStream)
-                {
-                    var buf = new char[blockSize];
-                    var totalRead = sr.ReadBlock(buf, 0, blockSize);
-                    data.Append(buf);
-                    if (DateTime.Now - startDownloadTime > TimeSpan.FromSeconds(1))
-                    {
-                        startDownloadTime = DateTime.Now;
-                        logger.WriteEntry(string.Format("Downloaded {0:#,##0} bytes so far", data.Length), LogType.Info);
-                    }
-                }
-
-                var obj = JObject.Parse(new StringReader(data.ToString()).ReadToEnd());
-                var channels = (JArray)obj["data"];
-
-                foreach (var channel in channels)
-                {
-                    var programs = channel["Programs"];
-                    var channelName = _channelsDict[channel.Value<int>("channel_id")];
-                    foreach (var program in programs)
-                    {
-                        var show = new Show();
-                        show.Channel = channelName;
-                        show.Title = Tools.CleanupText(program["name"].ToString());
-                        show.StartTime = DateTime.Parse(program["start_date"].ToString()).ToUniversalTime();
-                        show.EndTime = DateTime.Parse(program["end_date"].ToString()).ToUniversalTime();
-                        shows.Add(show);
-                    }
-
-                }
-            }
-            return shows;
-        }
-
-        Dictionary<int, string> _channelsDict;
-
+    {   
         public override List<Show> Grab(string xmlParameters, ILogger logger)
         {
-            var shows = new List<Show>();
-            var doc = XDocument.Parse(xmlParameters);            
-            var sdElement = doc.Descendants("StartDate").FirstOrDefault();
-            var startDateDiff = sdElement != null && sdElement.Value != null ? Convert.ToInt32(sdElement.Value) : -1;
-            var edElement = doc.Descendants("EndDate").FirstOrDefault();
-            var endDateDays = edElement != null && edElement.Value != null ? Convert.ToInt32(edElement.Value) : 3;
-
-            _channelsDict = GetChannelsDict(logger);
-
-            var maxTries = 15;
-            var retryCount = 0;
-
-            for (int i = startDateDiff; i < endDateDays; i++)
-            {
-                try
-                {
-                    var p = new GrabParameters() { Date = DateTime.Now.Date.AddDays(i) };
-                    shows.AddRange(Grab(p, logger));
-                    
-                }
-                catch (Exception ex)
-                {
-                    if (retryCount < maxTries)
-                    {
-                        retryCount++;
-                        logger.WriteEntry($"{ex.Message.ToLower()} , retry #{retryCount}", LogType.Warning);
-                        i--;
-                        continue;
-                    }
-                    else
-                        throw;
-                }
-            }
+            var shows = new List<Show>();            
+            FillGlobalShowsData(shows);
             return shows;
         }
 
-        private Dictionary<int, string> GetChannelsDict(ILogger logger)
+        private void FillGlobalShowsData(List<Show> shows)
         {
-            var wr = WebRequest.CreateHttp("https://pl.canalplus.com/iapi/tv-schedule/AllChannels");
-            try
+            var wr = WebRequest.CreateHttp("https://secure-webtv-static.canal-plus.com/metadata/cppol/all/v2.2/globalchannels.json"); // holds only current day data few hours forward
+            using (var res = (HttpWebResponse)wr.GetResponse())
             {
-                var res = (HttpWebResponse)wr.GetResponse();
-
-                var dict = new Dictionary<int, string>();
-                var obj = JObject.Parse(new StreamReader(res.GetResponseStream()).ReadToEnd())["data"];
-                foreach (var channel in obj)
+                var channels = JObject.Parse(new StreamReader(res.GetResponseStream()).ReadToEnd())["channels"];
+                foreach (var chan in channels)
                 {
-                    dict[channel.Value<int>("id")] = channel.Value<string>("name");
+                    foreach (var ev in chan["events"])
+                    {
+                        var show = new Show();
+                        show.Channel = chan.Value<string>("name");                        
+                        show.Title = ev.Value<string>("title");
+                        show.Description = ev.Value<string>("subTitle");
+                        show.StartTime = ev.Value<DateTime>("startDate");
+                        show.EndTime = ev.Value<DateTime>("endDate");
+                        shows.Add(show);
+                    }
                 }
-                return dict;
             }
-            catch (Exception ex)
-            {
-                logger.LogException(ex, "failed to get cyfra channels");
-                throw;
-            }
-        }
-    }
-
-    public class GrabParameters : GrabParametersBase
-    {
-        public DateTime Date { get; set; }
-    }
+        }        
+    }    
 }
