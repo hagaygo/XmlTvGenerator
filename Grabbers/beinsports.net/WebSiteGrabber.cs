3f0b7b55e4d11818948e830ff0924a79de11f728
hagaygo <hagay@HAGAY-PC>
2025-11-19 22:57:21 +0200

    updated bein sports grabber

--- Grabbers/beinsports.net/WebSiteGrabber.cs
+++ Grabbers/beinsports.net/WebSiteGrabber.cs
@@ -1,6 +1,8 @@
 ﻿using HtmlAgilityPack.CssSelectors.NetCore;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -11,18 +13,20 @@ using XmlTvGenerator.Core;
 
 namespace beinsports.net
 {
+    public class ChannelData
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
     public class WebSiteGrabber : GrabberBase
     {
-        //https://epg.beinsports.com/utctime_au.php?cdate=2020-09-13&offset=0&mins=00&category=sports&id=123
-        //https://epg.beinsports.com/utctime.php?cdate=2020-09-13&offset=0&mins=00&category=sports&id=123
-
-        const string URL = "https://epg.beinsports.com/utctime{0}.php?cdate={1}&offset=0&mins=00&category=sports&id=123";
-        const string DateFormat = "yyyy-MM-dd";
-
-        List<string> regions = new List<string> { "", "_au" };
-
         public override List<Show> Grab(string xmlParameters, ILogger logger)
         {
+            logger.WriteEntry($"Grabbing bein sports", LogType.Info);
+
+            const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0";
+
             var shows = new List<Show>();
 
             var doc = XDocument.Parse(xmlParameters);
@@ -30,70 +34,55 @@ namespace beinsports.net
             var startDateDiff = sdElement != null && sdElement.Value != null ? Convert.ToInt32(sdElement.Value) : 0;
             var edElement = doc.Descendants("EndDate").FirstOrDefault();
             var endDateDays = edElement != null && edElement.Value != null ? Convert.ToInt32(edElement.Value) : 3;
-            for (int i = startDateDiff; i <= endDateDays; i++)
-            {
-                var date = DateTime.Now.Date.AddDays(i);
-                foreach (var region in regions)
-                {
-                    try
-                    {
-                        var wr = WebRequest.Create(string.Format(URL, region, date.ToString(DateFormat)));
-                        logger.WriteEntry(string.Format("Grabbing bein sports region {0} , date {1} ...", region, date.ToString(DateFormat)), LogType.Info);
-                        var res = (HttpWebResponse)wr.GetResponse();
-                        var html = new HtmlAgilityPack.HtmlDocument();
-                        using (var data = res.GetResponseStream())
-                        {
-                            html.Load(data);                            
-                            var channelsDivs = html.QuerySelectorAll("div.container div div[id^=channels_]");
-                            foreach (var channelDiv in channelsDivs)
-                            {
-                                var img = channelDiv.QuerySelector("div.channel div.centered img");
-                                var src = img.Attributes["src"].Value;
-                                src = src.Substring(src.LastIndexOf("/") + 1);
-                                var channelName = src.Substring(0, src.IndexOf(".")) + region;
+            var region = doc.Descendants("Region").First().Value;
+            var dateFormat = "yyyy-MM-ddTHH:mm:ss.999Z";
+            var startDateText = HttpUtility.UrlEncode(DateTime.UtcNow.Date.AddDays(startDateDiff).ToString(dateFormat));
+            var endDateText = HttpUtility.UrlEncode(DateTime.UtcNow.Date.AddDays(endDateDays).ToString(dateFormat));
 
-                                foreach (var li in channelDiv.QuerySelectorAll("div.slider ul li"))
-                                {
-                                    if (li.QuerySelector("p.title") != null)
-                                    {
-                                        var title = HttpUtility.HtmlDecode(li.QuerySelector("p.title").InnerText);
-                                        var format = HttpUtility.HtmlDecode(li.QuerySelector("p.format").InnerText);
-                                        var time = HttpUtility.HtmlDecode(li.QuerySelector("p.time").InnerText);
-                                        var live = li.QuerySelectorAll("img.image_live_css").Count > 0;
-                                        if (live)
-                                            title = "Live - " + title;
-                                        var startTime = time.Substring(0, time.IndexOf("-") - 1).Trim();
-                                        var endTime = time.Substring(time.IndexOf("-") + 1).Trim();
-                                        var show = new Show();
-                                        show.Channel = channelName;
-                                        show.Title = title;
-                                        show.Description = format;
-                                        show.StartTime = date + DateTime.Parse(startTime).TimeOfDay;
-                                        show.StartTime = DateTime.SpecifyKind(show.StartTime, DateTimeKind.Utc);
-                                        show.EndTime = date + DateTime.Parse(endTime).TimeOfDay;
-                                        show.EndTime = DateTime.SpecifyKind(show.EndTime, DateTimeKind.Utc);
-                                        if (show.StartTime > show.EndTime)
-                                            show.StartTime = show.StartTime.AddDays(-1);
-                                        shows.Add(show);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    catch (Exception ex)
+            // get channels
+            var channels = new List<ChannelData>(); 
+            var wr = (HttpWebRequest)WebRequest.Create("https://www.beinsports.com/api/opta/tv-channel?region=" + region);
+            wr.UserAgent = UserAgent;
+            using (var response = wr.GetResponse())
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                var json = sr.ReadToEnd();
+                var data = JObject.Parse(json);
+                foreach (var row in data["rows"])
+                {
+                    channels.Add(new ChannelData { Name = row["name"].Value<string>(), Id = row["id"].Value<string>() });
+                }
+            }
+            var channelDict = channels.ToDictionary(x => x.Id);
+
+            var channelIds = string.Join("&",channels.Select(x => "channelIds=" + x.Id));
+            var url = $"https://www.beinsports.com/api/opta/tv-event?searchKey=&startBefore={endDateText}&endAfter={startDateText}&limit=3000&" + channelIds;
+            wr = (HttpWebRequest)WebRequest.Create(url);
+            wr.UserAgent = UserAgent;
+            using (var response = wr.GetResponse())
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                var json = sr.ReadToEnd();
+                var data = JObject.Parse(json);
+                foreach (var row in data["rows"])
+                {                    
+                    var channelId = row["channel"]["id"].Value<string>();
+                    if (channelDict.TryGetValue(channelId, out var channel))
                     {
-                        logger.LogException(ex);
+                        var s = new Show();
+                        s.Channel = channel.Name;
+                        s.Title = row["title"].Value<string>();
+                        s.Description = row["description"].Value<string>();
+                        s.StartTime = row["startDate"].Value<DateTime>();
+                        s.EndTime = row["endDate"].Value<DateTime>();
+                        if (row["live"].Value<bool>())
+                            s.Title = $"(Live) {s.Title}";
+                        shows.Add(s);
                     }
                 }
             }
-            var lst = new List<Show>();
-            foreach (var chan in shows.Select(x => x.Channel).Distinct())
-            {
-                var channelShows = shows.Where(x => x.Channel == chan).OrderBy(x => x.StartTime).ToList();
-                FixShowsEndTimeByStartTime(channelShows);
-                lst.AddRange(channelShows);
-            }
-            return lst;
+            logger.WriteEntry($"bein sports found {shows.Count} shows", LogType.Info);
+            return shows;
         }
     }
 }
